3df05c4c5a47556f8374318e408d6329a324ca9d
RealityRemix <realityremixroblox@gmail.com>
2025-01-20 15:38:41 -0500

    Update BlackScreenFix - Bloxstrap.bat

--- BlackScreenFix - Bloxstrap.bat
+++ BlackScreenFix - Bloxstrap.bat
@@ -44,7 +44,7 @@ reg add "HKCU\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers
 reg add "HKCU\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" /v "%path2%" /t REG_SZ /d "~ DISABLEDXMAXIMIZEDWINDOWEDMODE" /f
 
 cls
-echo %green%Black screen fixed, try injecting Synapse Z.%reset_color%
+echo %green%Black screen fixed, try using Swift.%reset_color%
 echo Press any key to exit...
 pause >nul
 exit /b 0
